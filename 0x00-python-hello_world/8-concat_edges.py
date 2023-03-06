96d3c3bfa2ccb0e30b1613890cda77420b8785eb
Gilbert Onyinyechi <gilbertonyinyechi1@gmail.com>
2023-03-06 13:31:37 -0800

    updated

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-str = str[34:60] + str[96:100] + str[:6]
+ language that combines remarkable power with very clear syntax"
+str =  str[32:57] + [96:10] + [:6]
 print(str)
