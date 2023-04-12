9b249ed2821534722885038fd881f66b67fe0a6a
Gilbert Onyinyechi <gilbertonyinyechi1@gmail.com>
2023-04-12 10:19:11 -0700

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function that returns the list of available attributes and methods of an object"""
+
+
+class lookup:
+    def __init__(self, list):
+        self.list =list
+
+        def __isattribute(self):
+            return self.list
+
+        list = lookup(attribute)
+        print(list)
