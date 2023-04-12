6a3ba4ad501fe99d8671ce9ccde8af8b6b02fb4d
Gilbert Onyinyechi <gilbertonyinyechi1@gmail.com>
2023-04-12 14:41:30 -0700

    inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This function returns true  if the object is an instance or false"""
+
+
+def is_same_class(obj, a_class):
+    """Returns if object is exactly an instance or false"""
+    return type(obj) == a_class
