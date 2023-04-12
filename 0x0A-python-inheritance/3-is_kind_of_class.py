753c8dfdbbd9eabef4d5d4003e6499e3fd9299e3
Gilbert Onyinyechi <gilbertonyinyechi1@gmail.com>
2023-04-12 14:51:54 -0700

    inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This returns True if the object is an instance or inherited"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function to return"""
+    return type(obj, a_class)
