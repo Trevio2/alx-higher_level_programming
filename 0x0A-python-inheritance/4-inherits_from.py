4256915d81ece24f5c237198863754c637725ad0
Gilbert Onyinyechi <gilbertonyinyechi1@gmail.com>
2023-04-12 15:11:40 -0700

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def is_kind_of_class(obj, a_class):
     """This function returns True if the object is an instance of a class"""
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class)
